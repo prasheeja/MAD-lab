dd5b3fb262447319897b6a7ae70a2fe9b9c27158
prasheeja <chn24mc038@ceconline.edu>
2025-09-11 15:11:15 +0530

    Add files via upload

--- cycle3/MainActivity.java
+++ cycle3/MainActivity.java
@@ -1,42 +1,26 @@
-package com.example.optionmenu;
+package com.example.explicitintent;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button navigateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigateButton = findViewById(R.id.navigateButton);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(explicitIntent);
+            }
+        });
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
     }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        int id=item.getItemId();
-        if(id==R.id.action_activity_one){
-         Intent intent=new Intent(MainActivity.this,activityOne.class);
-         startActivity(intent);
-            Toast.makeText(this, "Navigating to Activity One", Toast.LENGTH_SHORT).show();
-           return true;
-        }
-        else if(id==R.id.action_activity_two){
-            Intent intent=new Intent(MainActivity.this, activityTwo.class);
-           startActivity(intent);
-           Toast.makeText(this,"Navigating to Activity Two",Toast.LENGTH_SHORT).show();
-           return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
