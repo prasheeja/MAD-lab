1b6c82e9a3ef1af5a0f11127ef7cdf50d6b2d3ac
prasheeja <chn24mc038@ceconline.edu>
2025-09-11 15:08:33 +0530

    Add files via upload

---
+++ cycle3/menu_main.java
@@ -0,0 +1,14 @@
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_main);
+    }
+}
